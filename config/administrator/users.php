0a0f178ffc990f2d092c5cbf53f78d7fbe7f7734
vagrant <vagrant@homestead>
2018-12-14 07:31:40 +0000

    add administrator

---
+++ config/administrator/users.php
@@ -0,0 +1,52 @@
+<?php
+use App\Models\User;
+
+return [
+    'title' =>  '用户',
+    'heading' =>  '用户管理',
+    'single' => '用户',
+    'model' => User::class,
+    'columns' => [
+        'id' => [
+            'title' => 'ID'
+        ],
+        'name' => [
+            'title' => '用户名',
+        ],
+        'email' => [
+            'title' => 'Email',
+        ],
+        'created_at',
+        'operation' => [
+            'title'  => '管理',
+            'output' => function ($value, $model) {
+                return $value;
+            },
+            'sortable' => false,
+        ],
+    ],
+    'edit_fields' => [
+        'name' => [
+            'title' => '用户名',
+            'type' => 'text'
+        ],
+        'email' => [
+            'title' => 'Email',
+        ],
+        'password' =>[
+            'title' => '密码',
+            'type' => 'password'
+        ]
+    ],
+    'filters' => [
+        'id' => [
+            'title' => 'ID',
+        ],
+        'name' => [
+            'title' => '用户名',
+        ],
+        'email' => [
+            'title' => 'Email',
+        ],
+    ],
+];
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Authentication Routes...
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
