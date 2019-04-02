1d8f7cc8ea543e2639a3fb4dc7b90ecfe6882b39
guo xiao <guoxiao@buaa.edu.cn>
2019-04-02 12:06:43 +0800

    species update

--- app/Transformers/DatasSpecieTransformer.php
+++ app/Transformers/DatasSpecieTransformer.php
@@ -11,11 +11,12 @@ class DatasSpecieTransformer extends TransformerAbstract
     {
       return [
           'id' => $specie->id,
+          'species_id'=>$specie->species_id,
           'type' =>$specie->type,
           'name' =>$specie->name,
           'latin_name' =>$specie->latin_name,
           'code' =>$specie->code,
-          'uploaded_at' => $specie->uploaded_at,
+          'upload_at' => $specie->upload_at,
           'data' => json_decode($specie->data),
       ];
     }
